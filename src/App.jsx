c342b93cdfba1e2b27ceb741fa4e8518fe18aa53
Daniel Yo <danielyo112@gmail.com>
2024-03-07 05:21:20 +0000

    lesson 4

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Daniel Yo",
+  title: "Keyboard warrior",
+  email: "daniel.yo@neudesic.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "daniel-yo",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#00467F";
+const secondaryColor = "#F4802B";
 
 const App = () => {
   return (
